84338259049219312afaa92301fd47a46308bee4
SiyanTao <tt1512563556@gmail.com>
2021-04-02 12:52:24 +0800

    Add password check program

---
+++ pract_03/password_check.py
@@ -0,0 +1,12 @@
+def main():
+    password = get_password()
+    get_length = len(password)
+    print("*" * get_length)
+
+
+
+def get_password():
+    password = input("Please enter your password: ")
+    return password
+
+main()
