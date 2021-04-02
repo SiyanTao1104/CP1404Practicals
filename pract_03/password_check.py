ca33d854f186c1e61fd4bcb615bedf2929a06cc9
SiyanTao <tt1512563556@gmail.com>
2021-04-02 12:58:33 +0800

    Refactor password check program to use functions

--- pract_03/password_check.py
+++ pract_03/password_check.py
@@ -1,12 +1,16 @@
 def main():
     password = get_password()
     get_length = len(password)
-    print("*" * get_length)
+    print_length(get_length)
 
+
+def print_length(get_length):
+    print("*" * get_length)
 
 
 def get_password():
     password = input("Please enter your password: ")
     return password
+
 
 main()
