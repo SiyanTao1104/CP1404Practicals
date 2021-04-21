b75994c79ca67436559421e0c55c8a67c9025bb6
SiyanTao <tt1512563556@gmail.com>
2021-04-21 17:08:59 +0800

    create guitar objects in the guitars.py program. Test work and make sure it can make and view named guitars.

---
+++ pract_06/guitars.py
@@ -0,0 +1,37 @@
+"""CP1404/CP5632 Siyan Tao
+Practical -
+Playing the guitars
+"""
+from pract_06.guitar import Guitar
+
+guitars = []
+
+
+def main():
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        add_guitar = Guitar(name, year, cost)
+        guitars.append(add_guitar)
+        print("{} ({}) : ${:,.2f} added".format(name, year, cost))
+        name = input("Name: ")
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print("Guitar {0}: {1.name:>30} ({1.year}), worth ${1.cost:10,.2f}\
+             {2}".format(i + 1, guitar, vintage_string))
+    else:
+        print("No guitars :( Quick, go and buy one!")
+
+
+if __name__ == '__main__':
+    main()
