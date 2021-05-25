2b9085d906b384d9e7afb83b7a68dad292b616a5
SiyanTao <tt1512563556@gmail.com>
2021-05-25 17:19:57 +0800

    reformat the file name consistently

--- pract_09/cleanup_files.py
+++ pract_09/cleanup_files.py
@@ -7,40 +7,29 @@ import os
 
 
 def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
 
-    # Change to desired directory
-    os.chdir('Lyrics/Christmas')
+    os.chdir('Lyrics')
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
 
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
 
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
 
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
 
 
 
 
 def get_fixed_filename(filename):
-    """Return a 'fixed' version of filename."""
+    """returns a fixed filename"""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
     return new_name
 
 
-def demo_walk():
-    """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
-    for directory_name, subdirectories, filenames in os.walk('.'):
-        print("Directory:", directory_name)
-        print("\tcontains subdirectories:", subdirectories)
-        print("\tand files:", filenames)
-        print("(Current working directory is: {})".format(os.getcwd()))
+
 
 
 main()
